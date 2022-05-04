f75ba47c3406b39d054bee98bc3586f0c39ef912
Drissha <rian.alfazri96@gmail.com>
2022-05-04 20:14:13 +0700

    all done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('#karya').owlCarousel({
+        loop:true,
+        margin:10,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
+
+var owl = $('#karya');
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
+
+$(document).ready(function(){
+    $('#testimoni').owlCarousel({
         loop:true,
         margin:10,
         responsive:{
@@ -15,15 +45,3 @@ $(document).ready(function(){
         }
     });
 });
-
-var owl = $('.owl-carousel');
-// Go to the next item
-$('.customNextBtn').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-// Go to the previous item
-$('.customPrevBtn').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [300]);
-})
